75baded403d0a9e1fcad9a8fc4708be8ecf38722
23141669+noemialta@users.noreply.github.com <noemi.tavares2@gmail.com>
2019-04-19 11:31:36 -0300

    Adicionando o hero-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
     
   ],
   imports: [
